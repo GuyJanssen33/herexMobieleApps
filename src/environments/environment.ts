39585a647dd93914347711ff91759510c4aa8087
GuyJanssen33 <r0237357@student.thomasmore.be>
2023-08-10 13:30:05 +0200

    keystore werkt niet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBn4EbK_xP9naY2x-kg5V7JXDaEnbzQsis",
-    authDomain: "moestuin-app-guy-janssen.firebaseapp.com",
-    projectId: "moestuin-app-guy-janssen",
-    storageBucket: "moestuin-app-guy-janssen.appspot.com",
-    messagingSenderId: "646823487710",
-    appId: "1:646823487710:web:bfb610d15d891c4e0d7a4f"
+
   }
 };
 
